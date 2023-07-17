df4e2195e0524f48f0332497f3ea540afcec5e67
Li Sky <skyli@skyli.xyz>
2023-07-17 18:23:02 +0800

    update???

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-"use server";
+"use client";
 
 import Image from 'next/image'
 import Button from '@mui/material/Button';
@@ -7,20 +7,19 @@ import axios from 'axios';
 
 
 
-export async function SetOn() {
+ function SetOn() {
   async function openBox(e) {
     e.preventDefault()
     axios.get('http://localhost:5000/open')
   }
 
   return (
-    <div onclick ={openBox}>
+    <div onClick ={openBox}>
       <Button variant="contained"  >打开药箱</Button>
     </div>
   )
 }
-
-export async function SetOff(params) {
+ function SetOff(params) {
   // let onclick = () => {
   //   axios.get('http://localhost:5000/close')
   // }
@@ -31,43 +30,40 @@ export async function SetOff(params) {
 
 
   return (
-    <div onclick={closeBox}>
+    <div onClick={closeBox}>
       <Button variant="contained">关闭药箱</Button>
     </div>
   )
 }
 
-export async function SendAudio(params) {
+ function SendAudio(params) {
   async function uploadFile(e) {
-    e.preventDefault()
-    // select file
-    const file = document.getElementById('audio').files[0];
-    // create form data
+   e.preventDefault(); 
+   const handleFileUpload = async (event) => {
+    const file = event.target.files[0];
     const formData = new FormData();
-    // append data
     formData.append('file', file);
-    // post request
-
-    axios.post('http://localhost:5000/upload', formData, {
+    await axios.post('http://localhost:5000/upload', formData, {
       headers: {
         'Content-Type': 'multipart/form-data'
       }
-    }).then((response) => {
-      console.log(response)
-    }).catch((error) => {
-      console.log(error)
-    }
-    )
+    });
+  };
 
+  return (
+    <div>
+      <input type="file" onChange={handleFileUpload} />
+    </div>
+  );
   }
   return (
-    <div onclick={uploadFile}>
+    <div onClick={uploadFile}>
       <Button variant="contained">播放音频</Button>
     </div>
   )
 }
 
-export async function Home() {
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -79,3 +75,5 @@ export async function Home() {
     </main>
   )
 }
+
+
